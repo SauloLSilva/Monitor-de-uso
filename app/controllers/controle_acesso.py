fbc0c0a6592cb4b01e22ef424842352aa7f459e0
SauloLFSilva <ssilva@autodoc.com.br>
2023-08-07 20:25:36 -0300

    coleta de dados do mongodb e exibição em html

--- app/controllers/controle_acesso.py
+++ app/controllers/controle_acesso.py
@@ -1,7 +1,16 @@
 #importando o app do projeto principal
 from app import app
 from flask import render_template
+from app.models.mongodb import Mongodatabase
+from datetime import datetime
+
+mongodb = Mongodatabase()
+def get_first_digit(number):
+    return int(str(number)[0])
 
 @app.route('/controle_acesso')
 def controle_acesso():
-    return render_template('controle_acesso.html')
+    data_atual = datetime.now().strftime('%d-%m-%y')
+    usuarios = [mongodb.get_registro_de_uso(data_atual)]
+    data = [{"nome": item["nome"], "tempo_de_uso": get_first_digit(item["tempo_de_uso"])} for item in usuarios]
+    return render_template('controle_acesso.html', data=data)
