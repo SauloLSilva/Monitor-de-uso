668ffaabefc64468ce24d6d87f61af055a5189a6
“Saulo <ssilva@autodoc.com.br>
2023-08-13 12:13:28 -0300

    adicionado validação de cadastro, cadastro de usuario e reformulação de dados ao identificar faces

--- app/models/mongodb.py
+++ app/models/mongodb.py
@@ -11,21 +11,41 @@ class Mongodatabase(object):
         collection = db['users']
         return collection
     
-    def insert_dados(self, data, usuario, tempo):
-        collection = self.server()[data]
+    def validacao_cadastro(self, usuario):
+        collection = self.server()['cadastros']
+        query = {'nome': usuario}
+        registro_usuario = collection.count_documents(query)
+        if registro_usuario:
+            return True
+        else:
+            return False
+        
+    def cadastro_usuario(self, usuario, idade):
+        collection = self.server()['cadastros']
         dados = [
-            {'nome':usuario, 'tempo_de_uso':tempo/60},
+            {'nome':usuario, 'idade':idade},
         ]
         collection.insert_many(dados)
 
-    def get_dados(self, data, usuario, tempo):
+
+    def insert_dados_acesso(self, data, usuario, tempo):
         collection = self.server()[data]
         query = {'nome': usuario}
         registro_usuario = collection.count_documents(query)
         if registro_usuario:
             update = collection.update_one({'nome': usuario}, {'$inc': {'tempo_de_uso':tempo/60}})
         else:
-            self.insert_dados(data, usuario, tempo)
+            dados = [
+                {'nome':usuario, 'tempo_de_uso':tempo/60},
+            ]
+            collection.insert_many(dados)
+
+    def get_dados_acesso(self, data, usuario, tempo):
+        collection = self.server()['cadastros']
+        query = {'nome': usuario}
+        registro_usuario = collection.count_documents(query)
+        if registro_usuario:
+            self.insert_dados_acesso(data, usuario, tempo)
 
     def get_registro_de_uso(self, data):
         collection = self.server()[data]
