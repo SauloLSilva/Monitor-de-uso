9c6b3ee2cec971de370baf31013f40226049b31a
“Saulo <ssilva@autodoc.com.br>
2023-08-13 12:13:56 -0300

    comentado função de monitoramento, ainda em desenvolvimento

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def run_process(process):
     os.system('python3 {0}'.format(process))
     time.sleep(2.5)
 
-process = ('identificador.py', 'run.py','monitoramento.py')
+# process = ('identificador.py', 'run.py','monitoramento.py')
+# pool = Pool(processes=3)
+process = ('identificador.py', 'run.py')
 pool = Pool(processes=2)
 pool.map(run_process, process)
