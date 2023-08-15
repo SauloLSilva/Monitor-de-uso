69fd1aeba041058d9176ff696b5424b93bd982c0
SauloLFSilva <ssilva@autodoc.com.br>
2023-08-15 19:52:39 -0300

    função para envio de relatório

--- app/controllers/controle_acesso.py
+++ app/controllers/controle_acesso.py
@@ -1,15 +1,21 @@
 #importando o app do projeto principal
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from app.models.mongodb import Mongodatabase
 from datetime import datetime
+from app.models.alertas import alertas
 
 mongodb = Mongodatabase()
+alerta = alertas()
+
 def get_first_digit(number):
     return int(str(number).split('.')[0])
 
-@app.route('/controle_acesso')
+@app.route('/controle_acesso', methods=['GET', 'POST'])
 def controle_acesso():
+    if request.method == 'POST':
+        alerta.monitoramento_hora_dia('dia')
+        return redirect(url_for('pagina_inicial'))
     try:
         data_atual = datetime.now().strftime('%d-%m-%y')
         usuarios = [mongodb.get_registro_de_uso(data_atual)]
