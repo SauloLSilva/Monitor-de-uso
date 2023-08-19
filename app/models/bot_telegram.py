ae5af78f2a767ebfb869071c14de60805e6c8297
“Saulo <ssilva@autodoc.com.br>
2023-08-19 19:20:22 -0300

    envio de realatório gráfico por telegram

--- app/models/bot_telegram.py
+++ app/models/bot_telegram.py
@@ -1,15 +1,29 @@
 import telegram
+import subprocess
 
 class Bot_alerta(object):
+    def token_api(self):
+        # Defina o token de acesso do seu bot aqui
+        return '6089265882:AAGwlx8NAq4RC4QbVLPeSb_nouICZJF98Yk', '-914116709'
+
     def envio_de_alerta(self, mensagem):
 
-        # Defina o token de acesso do seu bot aqui
-        token = '6089265882:AAGwlx8NAq4RC4QbVLPeSb_nouICZJF98Yk'
-
         # Crie um objeto bot com o seu token de acesso
-        bot = telegram.Bot(token)
+        bot = telegram.Bot(self.token_api()[0])
 
         # Envie uma mensagem para o seu bot
-        bot.send_message(chat_id='-914116709', text=mensagem)
+        bot.send_message(self.token_api()[1], text=mensagem)
         
         return True
+    
+    def envio_imagem(self):
+        rota = str(subprocess.check_output(['pwd']).decode('utf-8'))[:-1]
+        rota = f'{rota}/app/static/graphs/relatorio_diario.png'
+
+        # Crie um objeto bot com o seu token de acesso
+        bot = telegram.Bot(self.token_api()[0])
+
+        # Envie uma mensagem para o seu bot
+        bot.send_photo(self.token_api()[1], photo=open(rota, 'rb'))        
+        return True
+        
