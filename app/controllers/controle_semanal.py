d8c57cfb5d1e6e693a03b1830c8fff30aba1e64a
“Saulo <ssilva@autodoc.com.br>
2023-09-03 19:55:40 -0300

    gerador de gráfico, relatório semanal

--- app/controllers/controle_semanal.py
+++ app/controllers/controle_semanal.py
@@ -1,7 +1,34 @@
 #importando o app do projeto principal
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+from app.models.mongodb import Mongodatabase
+from app.models.dashboards import Gerar_grafico
+from app.models.bot_telegram import Bot_alerta
 
-@app.route('/controle_semanal')
+
+mongodb = Mongodatabase()
+telegram = Bot_alerta()
+grafico = Gerar_grafico()
+
+@app.route('/controle_semanal', methods=['GET', 'POST'])
 def controle_semanal():
-    return render_template('controle_semanal.html')
+
+    if request.method == 'POST':
+        data = request.form
+        id = data.get('usuario_id')
+        dados = mongodb.get_id_colaborador(id)
+        grafico.grafico_individual(dados[0], dados[1])
+
+    try:
+        usuarios = mongodb.get_cadastros()
+        usuarios_data = []
+        for usuario in usuarios:
+            usuarios_data.append({
+                'id': usuario['_id'],
+                'nome': usuario['nome'],
+                'idade': usuario['idade']
+            })
+    except Exception as err:
+        usuarios_data = ''
+
+    return render_template('controle_semanal.html', data=usuarios_data)
--- app/models/dashboards.py
+++ app/models/dashboards.py
@@ -1,12 +1,18 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
-from datetime import datetime
+from datetime import datetime, timedelta
 from app.models.mongodb import Mongodatabase
+import subprocess
 
 class Gerar_grafico(object):
 
     def grafico_diario(self):
+
+        try:
+            subprocess.check_output(['rm', 'app/static/graphs/relatorio_diario.png'])
+        except:
+            pass
 
         data_atual = datetime.now().strftime('%d-%m-%y %H:%M')
         cores = list()
@@ -74,6 +80,84 @@ class Gerar_grafico(object):
 
             
             plt.savefig ('app/static/graphs/relatorio_diario.png')
+            plt.close()
+        except Exception as err:
+            print(err)
+
+    def grafico_individual(self, nome, idade):
+
+        try:
+            subprocess.check_output(['rm', 'app/static/graphs/monitoramento_individual.png'])
+        except:
+            pass
+
+        verde = '#008000'
+        amarelo = "#FFD700"
+        vermelho = "#8B0000"
+        azul = '#1a1a79'
+        paleta_de_cores = [verde, amarelo, vermelho, azul]
+        declaracao_cores = ['Bom', 'Elevado','Muito Elevado', '18+']
+
+        data_atual = datetime.now()
+        datas = [data_atual - timedelta(days=i+1) for i in range(6, -1, -1)]
+        get_dados_mongo = [data.strftime('%d-%m-%y') for data in datas]
+
+        dias_da_semana = get_dados_mongo
+        acesso_semanal = list()
+        cores = list()
+
+        for dia in get_dados_mongo:
+            data = Mongodatabase().get_registro_de_uso_semanal(dia, nome)
+            try:
+                for dados in data:
+                    tempo_de_uso = (str(dados['tempo_de_uso']).split('.')[0])
+                    acesso_semanal.append(tempo_de_uso)
+                    if idade >=2 and idade < 6:
+                        if tempo_de_uso >= 60 and tempo_de_uso <= 75:
+                            cores.append(amarelo)
+                        elif tempo_de_uso > 75:
+                            cores.append(vermelho)
+                        else:
+                            cores.append(verde)
+
+                    elif idade >=6 and idade <= 11:
+                        if tempo_de_uso >= 120 and tempo_de_uso >= 135:
+                            cores.append(amarelo)
+                        elif tempo_de_uso > 135:
+                            cores.append(vermelho)
+                        else:
+                            cores.append(verde)
+                    elif idade >=11 and idade <= 18:
+                        if tempo_de_uso >= 180 and tempo_de_uso >= 195:
+                            cores.append(amarelo)
+                        elif tempo_de_uso > 195:
+                            cores.append(vermelho)
+                        else:
+                            cores.append(verde)
+            except:
+                cores.append(verde)
+                acesso_semanal.append(0)
+
+        try:
+            sns.set(style="whitegrid")
+            plt.figure(figsize=(8, 6))
+            ax = sns.barplot(x=acesso_semanal, y=dias_da_semana, palette=cores)
+
+            plt.ylabel("Data")
+            plt.xlabel("Tempo(Minutos)")
+            plt.title(f"Tempo de uso do Usuário {nome}\n{dias_da_semana[0]} - {dias_da_semana[-1]}")
+
+            color_patches = [mpatches.Patch(color=color, label=label) for color, label in zip(paleta_de_cores, declaracao_cores)]
+
+            plt.legend(handles=color_patches, bbox_to_anchor=(1.02, 0.5), loc='center left')
+
+            plt.tight_layout()
+
+            for i, v in enumerate(acesso_semanal):
+                ax.text(v + 1, i, str(v), ha='left', va='center', fontsize=10)
+
+
+            plt.savefig ('app/static/graphs/monitoramento_individual.png')
             plt.close()
         except Exception as err:
             print(err)
