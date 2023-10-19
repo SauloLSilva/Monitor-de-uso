7fd53e8830fc9c74ec8cac8314cef8cd1ddc9739
Saulo Silva <90773957+SauloLFSilva@users.noreply.github.com>
2023-10-19 20:40:18 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,14 @@ import os
 app = Flask(__name__, static_url_path='/static')
 try:
     os.mkdir('app/static/img')
+except:
+    pass
+
+try:
     os.mkdir('app/static/graphs')
 except:
     pass
+    
 app.config['UPLOAD_FOLDER'] = 'app/static/img'
 
 from app.controllers import index, controle_semanal, pagina_inicial, controle_diario, cadastro_usuario, registro_facial
